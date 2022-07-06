2367c8da4df97d56ab7df79075aacf4ab9f81362
carreuky <carreuky@gmail.com>
2022-07-06 10:00:28 +0300

    creating addlistener to my  button calculate

--- assest/index.js
+++ assest/index.js
@@ -1,8 +1,16 @@
+let salary=document.getElementById('ksh')
+let button=document.getElementById('button')
+let income=document.getElementById('income')
+let bonus=document.getElementById('benefits')
+let kshbonus=document.getElementById('kshbonus')
+let nssfout=document.getElementById('nssfOut')
+let incomePensioned=document.getElementById('incomeAfterPension')
 
-
-
-
-
-
-
-
+button.addEventListener('click', ()=>{
+    totalIncome()
+    nsff()
+    incomeSubPension()
+    nhif()
+    // income.textContent=parseFloat(salary.value)
+    bonus.textContent=parseFloat(kshbonus.value)
+})
