921f0843aa0ec4bcde864e7dd21298276a2ca0bf
carreuky <carreuky@gmail.com>
2022-07-06 10:03:09 +0300

    single check for box

--- assest/index.js
+++ assest/index.js
@@ -14,3 +14,30 @@ button.addEventListener('click', ()=>{
     // income.textContent=parseFloat(salary.value)
     bonus.textContent=parseFloat(kshbonus.value)
 })
+
+//having single selection for the buttons
+function onlyOne(checkbox) {
+    let period= document.getElementsByName('period')
+    period.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+    incometax()
+}
+function checknssf(checkbox) {
+    let nssf= document.getElementsByName('nssf')
+    nssf.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+function yesOrNonssf(checkbox) {
+    let yesno= document.getElementsByName('yesno')
+    yesno.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+function checknhif(checkbox){
+    let yesno=document.getElementsByName('nhif')
+    yesno.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
